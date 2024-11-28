65d59a3f2d72a5b2b50107ba88321260d0207dab
unknown <chloe.legouas@hotmail.fr>
2024-11-28 21:51:27 +0100

    export accueil

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Home from './pages/home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Connexion from './pages/connexion';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<Connexion />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+      </Router>
+
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
---
+++ src/pages/home.js
@@ -0,0 +1,15 @@
+import '../styles/home.css';
+import '../styles/typo.css';
+
+
+
+function Home() {
+    return (
+      <div>
+        <h1>Mon projet React</h1>
+      </div>
+    );
+  }
+  
+  export default Home;
+  
